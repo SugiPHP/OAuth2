d5a263f05d2f52b4aca1266c78ac0e2a1e9ee9ed
Plamen Popov <tzappa@gmail.com>
2013-01-22 11:35:37 +0200

    added almost dummy page for authorization endpoint

---
+++ auth.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Authorization endpoint.
+ * The client (OAuth2 client) uses this endpoint to obtain authorization from the resource owner (user)
+ *
+ * @see http://tools.ietf.org/html/rfc6749#section-3.1
+ *
+ * @package OAuth2
+ * @version 13.01.22
+ */
+
+require "src/OAuth2.php";
+
+// According to the RFC, this MUST be the first thing to do: 
+// Auth server MUST verify the identity of the user.
+// OAuth2 standard does not describe how the user (resource owner) authenticates to the server.
+// Neither do we
+$user_id = 1;
+if (!$user_id) {
+	// instead of throwing an exception we can navigate the user to the login page
+	throw new \Exception("Unknown user");
+}
+
+$auth = new OAuth2;
+// check client request
+$request = $auth->authRequest();
+
+var_dump($request);
