b4a000269a004b37f8277cc5d9f47d4d895ac21f
Plamen Popov <tzappa@gmail.com>
2013-02-04 13:14:24 +0200

    adding unit testing frame - nothing in there!

---
+++ test/OAuth2Test.php
@@ -0,0 +1,50 @@
+<?php
+
+require __DIR__."/../lib/OAuth2.php";
+
+class OAuth2Test extends PHPUnit_Framework_TestCase
+{
+	public static function setUpBeforeClass()
+	{
+
+	}
+
+	protected function setUp()
+	{
+
+	}
+
+	protected function assertPreConditions()
+	{
+
+	}
+
+	protected function assertPostConditions()
+	{
+
+	}
+
+	protected function tearDown()
+	{
+
+	}
+
+	public static function tearDownAfterClass()
+	{
+
+	}
+
+	protected function onNotSuccessfulTest(Exception $e)
+	{
+		// ...
+		throw $e;
+	}
+
+
+	public function testConstructor()
+	{
+		// $servers = new OAuth2();
+
+		$this->markTestIncomplete("...");
+	}
+}
